a859dc7c2a544064e6e6867f4035d60b02ca73fd
Fernando Mateus de Oliveira <mateusjp15@gmail.com>
2018-11-16 15:20:16 -0300

    Delete Resource via API

--- src/main/java/com/oliveirafernando/controller/ReleaseController.java
+++ src/main/java/com/oliveirafernando/controller/ReleaseController.java
@@ -13,12 +13,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oliveirafernando.event.ResourceCreatedEvent;
@@ -75,5 +77,11 @@ public class ReleaseController {
 		List<Error> errors = Arrays.asList(new Error(errorMessage, technicalMessage));
 
 		return ResponseEntity.badRequest().body(errors);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable Long id) {
+		this.releaseRepository.deleteById(id);
 	}
 }
