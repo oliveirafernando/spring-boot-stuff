be1aed39977b06d8f92a5bb21dd428692c6b96f6
Fernando Mateus de Oliveira <mateusjp15@gmail.com>
2018-11-22 23:58:25 -0300

    Refreshing token only  based on Cookie 'access_token' property

---
+++ src/main/java/com/oliveirafernando/token/RefreshTokenCookiePreProcessorFilter.java
@@ -0,0 +1,75 @@
+package com.oliveirafernando.token;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.apache.catalina.util.ParameterMap;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RefreshTokenCookiePreProcessorFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		boolean isRequestToTokenURI = "/oauth/token".equalsIgnoreCase(req.getRequestURI());
+		boolean isGrantTypeRefreshToken = "refresh_token".equals(req.getParameter("grant_type"));
+		
+		if (isRequestToTokenURI && isGrantTypeRefreshToken && req.getCookies() != null) {
+			for (Cookie cookie : req.getCookies()) {
+				if (cookie.getName().equals("refreshToken")) {
+					String refreshToken = cookie.getValue();
+					req = new MyServletRequestWrapper(req, refreshToken);
+				}
+			}
+
+		}
+
+		chain.doFilter(req, response);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+	
+	static class MyServletRequestWrapper extends HttpServletRequestWrapper{
+
+		private String refreshToken;
+
+		public MyServletRequestWrapper(HttpServletRequest request, String refreshToken) {
+			super(request);
+			this.refreshToken = refreshToken;
+		}
+		
+		@Override
+		public Map<String, String[]> getParameterMap() {
+			ParameterMap<String, String[]> map = new ParameterMap<>(getRequest().getParameterMap());
+			map.put("refresh_token", new String[] { this.refreshToken });
+			map.setLocked(true);
+			
+			return map;
+		}
+	}
+
+}
